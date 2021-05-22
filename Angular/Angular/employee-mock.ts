3f48bcb8c4c314bd97360f7a519c7d56304c6e10
Blitzar9 <80390976+Blitzar9@users.noreply.github.com>
2021-05-22 16:11:48 +0530

    Add files via upload

---
+++ Angular/Angular/employee-mock.ts
@@ -0,0 +1,7 @@
+import {Employee} from './model/employee';
+
+export const EMPLOYEES: Employee[] = [
+{id: 101, password:'password', name: 'Akshay', address: 'Trichy', age: 21, company: 'CTS'},
+{id: 102, password:'password', name: 'Aishwarya', address: 'Banglore', age: 21, company: 'Google'},
+{id: 103, password:'password', name: 'Paval', address: 'Chennai', age: 22, company: 'Wipro'},
+];
